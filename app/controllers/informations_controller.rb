756afc5ee3f73f43fa09a6094142b3d9892f4e72
Your Name <forever.alone92@gmail.com>
2017-03-23 10:23:40 +0700

    wWrite information new page

--- app/controllers/informations_controller.rb
+++ app/controllers/informations_controller.rb
@@ -1,3 +1,22 @@
 class InformationsController < ApplicationController
 	
+	def new
+		@information = Information.new
+	end
+	
+	def create
+		@information = Information.new(information_params)
+		if @information.save
+			flash[:success] = "Your information has been saved"
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+	
+	private
+		
+		def information_params
+			params.require(:information).permit(:weight, :height, :gender, :activity_level, :user_id)
+		end
 end
